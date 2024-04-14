ef471e405cd50b23c8aba562ea276230f803c30e
ChrisRzech <christopherrzechula@gmail.com>
2024-04-13 23:06:14 -0700

    Add byte order utility and unit tests for it. Add hexdump utility.

---
+++ src/core/common/src/utility.cpp
@@ -0,0 +1,44 @@
+#include "chat/common/utility.hpp"
+
+#include <iomanip>
+
+namespace chat::common::utility
+{
+
+void hexdump(std::ostream& out, const std::byte* bytes, std::size_t size)
+{
+    constexpr std::size_t BYTES_PER_CHUNK = 2;
+    constexpr std::size_t BYTES_PER_LINE = 16;
+    std::array<char, BYTES_PER_LINE + 1> chars = {'\0'}; //Make sure to have null-terminator
+    std::size_t linePaddingCount = BYTES_PER_LINE - (size % BYTES_PER_LINE);
+
+    for(std::size_t i = 0; i < size + linePaddingCount; i++)
+    {
+        if(i % BYTES_PER_LINE == 0)
+        {
+            out << std::setw(8) << std::setfill('0') << std::hex << i << std::dec << std::setfill(' ') << "  ";
+        }
+
+        if(i < size)
+        {
+            out << std::setw(2) << std::setfill('0') << std::hex << static_cast<int>(bytes[i]) << std::dec << std::setfill(' ');
+            chars[i % BYTES_PER_LINE] = std::isprint(static_cast<int>(bytes[i])) != 0 ? static_cast<char>(bytes[i]) : '.';
+        }
+        else
+        {
+            out << "  ";
+            chars[i % BYTES_PER_LINE] = ' ';
+        }
+
+        if((i + 1) % BYTES_PER_LINE == 0)
+        {
+            out << " |" << chars.data() << "|\n";
+        }
+        else if((i + 1) % BYTES_PER_CHUNK == 0)
+        {
+            out << ' ';
+        }
+    }
+}
+
+}
---
+++ src/core/common/test/UtilityTest.cpp
@@ -0,0 +1,49 @@
+#include "chat/common/utility.hpp"
+
+#include <catch2/catch_template_test_macros.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+TEMPLATE_TEST_CASE("Converting to and from network byte order", "[byte order]", std::int8_t, std::uint8_t, std::int16_t, std::uint16_t,
+    std::int32_t, std::uint32_t, std::int64_t, std::uint64_t)
+{
+    //Testing byte order conversion is difficult since the machine usually can't switch the native byte ordering. Some architectures, like
+    //ARM (https://stackoverflow.com/a/28726501/21445636), can change byte ordering in the middle of execution. However, that assumption
+    //will not be used in testing here. Instead, it will be assumed that testing on a machine without such functionality is sufficient.
+
+    auto getExpectedNetworkBytes = [](auto value) constexpr
+    {
+        chat::common::ByteArray<sizeof(value)> bytes = {};
+        for(std::size_t i = 0; i < bytes.size(); i++)
+        {
+            bytes[i] = static_cast<std::byte>(i == bytes.size() - 1 ? value : 0);
+        }
+        return bytes;
+    };
+
+    GIVEN("An integral value")
+    {
+        using Integral = TestType;
+        constexpr Integral value = 42;
+        constexpr auto expectedNetworkBytes = getExpectedNetworkBytes(value);
+
+        WHEN("The value is converted to network byte order")
+        {
+            constexpr auto networkBytes = chat::common::utility::toNetworkByteOrder(value);
+
+            THEN("The result matches the expected network byte order")
+            {
+                CHECK(networkBytes == expectedNetworkBytes);
+            }
+
+            WHEN("The value is reconstructed to host byte order")
+            {
+                constexpr auto reconstructedValue = chat::common::utility::toHostByteOrder<Integral>(networkBytes);
+
+                THEN("The result matches the initial value")
+                {
+                    CHECK(reconstructedValue == value);
+                }
+            }
+        }
+    }
+}
