d6da0fc9df4914c9bc8ebc8530c92e5ae8fe9422
ChrisRzech <christopherrzechula@gmail.com>
2025-01-18 17:35:06 -0800

    Add strongly-typed Port type

---
+++ src/core/common/include/chat/common/Port.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstdint>
+
+namespace chat::common
+{
+/**
+ * @brief A type to represent a port number used for networking.
+ *
+ * @details The values of a port number ranged from 0 to 65,535.
+ */
+enum class Port : std::uint16_t {};
+}
